029105810b3ab7044fd3fb58f4197c37569f77b9
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-05-17 18:34:21 +0500

    feat: add authentication with django-allauth

---
+++ accounts/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+
+from accounts.forms import CustomUserChangeForm, CustomUserCreationForm
+
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+  add_form = CustomUserCreationForm
+  form = CustomUserChangeForm
+  model = User
+  list_display = ('email', 'username', 'is_superuser',)
+
+admin.site.register(User, CustomUserAdmin)
