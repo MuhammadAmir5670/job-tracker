5482c518719bc911bcbc80c75d33717f4b27e52e
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-05-19 16:26:34 +0500

    add jobs list page

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from jobs.models import Job
+
+
+class JobsListView(generic.ListView):
+    model = Job
+    template_name = "jobs/jobs_list.html"
+    context_object_name = "job_list"
