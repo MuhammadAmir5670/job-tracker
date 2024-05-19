158dbab8d40a6c85648bc053626b649516bf35de
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-05-19 16:25:13 +0500

    pluralize m2m tech_stack field

--- jobs/models.py
+++ jobs/models.py
@@ -4,7 +4,10 @@ from django.utils.translation import gettext_lazy as _
 
 from core.models import TimeStampedModel
 
+
 class TechStack(TimeStampedModel):
+    """Model definition for TechStack."""
+
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -15,21 +18,26 @@ class Job(TimeStampedModel):
     """Model definition for Job."""
 
     class Status(models.TextChoices):
-        WISHLIST = 'WISHLIST', _('Wishlist')
-        APPLIED = 'APPLIED', _('Applied')
-        INTERVIEW = 'INTERVIEW', _('Interview')
-        REJECTED = 'REJECTED', _('Rejected')
-        OFFER = 'OFFER', _('Offer')
-        DORMANT = 'DORMANT', _('Dormant')
-        FOLLOW_UP = 'FOLLOW_UP', _('Follow Up')
+        WISHLIST = "WISHLIST", _("Wishlist")
+        APPLIED = "APPLIED", _("Applied")
+        INTERVIEW = "INTERVIEW", _("Interview")
+        REJECTED = "REJECTED", _("Rejected")
+        OFFER = "OFFER", _("Offer")
+        DORMANT = "DORMANT", _("Dormant")
+        FOLLOW_UP = "FOLLOW_UP", _("Follow Up")
 
     title = models.CharField(max_length=500)
     company = models.CharField(max_length=500)
-    tech_stack = models.ManyToManyField(TechStack)
-    status = models.CharField(max_length=200, choices=Status.choices, default=Status.WISHLIST)
+    tech_stacks = models.ManyToManyField(TechStack)
+    status = models.CharField(
+        max_length=200, choices=Status.choices, default=Status.WISHLIST
+    )
     link = models.TextField(validators=[URLValidator()])
     applied_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Unicode representation of Job."""
         return self.title
+
+    def pretty_techstack(self):
+        return ", ".join(self.tech_stacks.values_list("name", flat=True))
