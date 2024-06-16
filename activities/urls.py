f3f939d8a69ee8d59cfd3ce9b96633c4fe1d56c4
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-06-16 19:41:02 +0500

    add activity types CRUD

--- activities/urls.py
+++ activities/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
-from .views import ActivityCreateView
+from .views import ActivityCreateView, ActivityTypeCreateView, ActivityTypeListView, ActivityTypeUpdateView
 
 urlpatterns = [
     path("<int:job_pk>", ActivityCreateView.as_view(), name="activity_create"),
+    path("types/", ActivityTypeListView.as_view(), name="activity_type_list"),
+    path("types/create", ActivityTypeCreateView.as_view(), name="activity_type_create"),
+    path(
+        "types/<int:pk>/update",
+        ActivityTypeUpdateView.as_view(),
+        name="activity_type_update",
+    ),
 ]
--- activities/views.py
+++ activities/views.py
@@ -1,9 +1,8 @@
 from django.urls import reverse
 from django.views import generic
 
-from activities.models import Activity
-
-from .forms import ActivityForm
+from .forms import ActivityForm, ActivityTypeForm
+from .models import Activity, ActivityType
 
 
 class ActivityCreateView(generic.CreateView):
@@ -19,3 +18,22 @@ class ActivityCreateView(generic.CreateView):
         form.instance.job_id = self.kwargs["job_pk"]
 
         return super().form_valid(form)
+
+
+class ActivityTypeListView(generic.ListView):
+    model = ActivityType
+    template_name = "activities/activity_type_list.html"
+    context_object_name = "activity_type_list"
+    paginate_by = 2
+
+
+class ActivityTypeCreateView(generic.CreateView):
+    model = ActivityType
+    template_name = "activities/activity_type_create.html"
+    form_class = ActivityTypeForm
+
+
+class ActivityTypeUpdateView(generic.UpdateView):
+    model = ActivityType
+    template_name = "activities/activity_type_update.html"
+    form_class = ActivityTypeForm
