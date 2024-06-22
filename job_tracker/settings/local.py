5a3cf5ce3de5437b848094d1da6df15a3b602613
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-06-22 20:09:12 +0500

    feedback: remove extra IP from `INTERNAL_IPS` list

--- job_tracker/settings/local.py
+++ job_tracker/settings/local.py
@@ -25,7 +25,7 @@ INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
 
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+INTERNAL_IPS = ["127.0.0.1"]
 if env("USE_DOCKER"):
     import socket
 
