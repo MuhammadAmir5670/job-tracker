1bd9440bd177f090adc376fb60638183d393e065
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-06-23 14:38:30 +0500

    add validation basd on job source

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from django_tomselect.widgets import TomSelectMultipleWidget
 
@@ -17,20 +19,29 @@ class JobForm(forms.ModelForm):
             "link": forms.TextInput,
         }
 
+    def clean(self):
+        self._validate_job_source_link()
+
+        return self.cleaned_data
+
+    def _validate_job_source_link(self):
+        job_link = self.cleaned_data.get("link")
+        job_source = self.cleaned_data.get("job_source")
+
+        if not job_source or not job_link:
+            return
+
+        if not re.match(job_source.link_regex, job_link):
+            self.add_error("link", f"Not a valid URL for the selected Job Source: {job_source}")
+
 
 class JobFilterForm(forms.Form):
     status_choices = [("", "------------")] + Job.Status.choices
 
     status = forms.ChoiceField(choices=status_choices, required=False)
-    start_date = forms.DateField(
-        required=False, widget=forms.DateInput(attrs={"type": "date"})
-    )
-    end_date = forms.DateField(
-        required=False, widget=forms.DateInput(attrs={"type": "date"})
-    )
-    tech_stacks = forms.ModelMultipleChoiceField(
-        TechStack.objects, required=False, widget=TechStackWidget()
-    )
+    start_date = forms.DateField(required=False, widget=forms.DateInput(attrs={"type": "date"}))
+    end_date = forms.DateField(required=False, widget=forms.DateInput(attrs={"type": "date"}))
+    tech_stacks = forms.ModelMultipleChoiceField(TechStack.objects, required=False, widget=TechStackWidget())
 
     def filter(self, queryset):
         if self.is_valid():
