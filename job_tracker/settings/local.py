d35c75fe1f10d04a3bbea5c1fe584ba1d8bcb400
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-06-28 18:10:50 +0500

    set console email backend for local env

--- job_tracker/settings/local.py
+++ job_tracker/settings/local.py
@@ -33,4 +33,9 @@ if env("USE_DOCKER"):
 # django-extensions
 # ------------------------------------------------------------------------------
 # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
-INSTALLED_APPS += ["django_extensions"]
+INSTALLED_APPS += ["django_extensions"]  # noqa: F405
+
+# Django Email Backend
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/5.0/topics/email/#console-backend
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
