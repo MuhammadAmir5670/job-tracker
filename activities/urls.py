5e15a543a9701817c47225a8f70161842726d921
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-06-29 22:41:58 +0500

    replace actions text with icons

--- activities/urls.py
+++ activities/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import ActivityCreateView, ActivityTypeCreateView, ActivityTypeListView, ActivityTypeUpdateView
 
 urlpatterns = [
-    path("<int:job_pk>", ActivityCreateView.as_view(), name="activity_create"),
+    path("<int:job_pk>", ActivityCreateView.as_view(), name="log_activity"),
     path("types/", ActivityTypeListView.as_view(), name="activity_type_list"),
     path("types/create", ActivityTypeCreateView.as_view(), name="activity_type_create"),
     path(
