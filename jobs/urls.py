14758ce377ee98486c8b226a74c8764ce374884e
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-07-06 16:44:40 +0500

    job: change list url name

--- jobs/urls.py
+++ jobs/urls.py
@@ -13,7 +13,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("jobs/", JobListView.as_view(), name="jobs_list"),
+    path("jobs/", JobListView.as_view(), name="job_list"),
     path("jobs/<int:pk>/", JobDetailView.as_view(), name="job_detail"),
     path("jobs/create/", JobCreateView.as_view(), name="job_create"),
     path("jobs/<int:pk>/update/", JobUpdateView.as_view(), name="job_update"),
