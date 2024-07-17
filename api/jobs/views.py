b2dd279c4a5ed7f798c5392eac079cedb5570d06
haseeb-ali-dev <h45.busy@gmail.com>
2024-07-18 01:02:28 +0500

    add pagination & search lookup to tech stack viewset

--- api/jobs/views.py
+++ api/jobs/views.py
@@ -1,12 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from jobs.models import TechStack
 
 from .serializers import TechStackSerializer
 from .permissions import TechStackPermission
+from .pagination import TechStackCursorPagination
 
 
 class TechStackViewSet(viewsets.ModelViewSet):
     queryset = TechStack.objects.all()
     serializer_class = TechStackSerializer
     permission_classes = [TechStackPermission]
+    pagination_class = TechStackCursorPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
