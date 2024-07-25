c091a77cd46c60af47c494ef6f5281c6f4adf491
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-07-25 04:04:23 -0700

    add JobSerializer

--- api/jobs/serializers.py
+++ api/jobs/serializers.py
@@ -1,6 +1,30 @@
+import re
+
 from rest_framework import serializers
 
-from jobs.models import TechStack
+from jobs.models import Job, TechStack
+
+
+class JobSerializer(serializers.ModelSerializer):
+    tech_stacks = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Job
+        fields = ("title", "company", "tech_stacks", "status", "link", "applied_at", "job_source")
+
+    def validate(self, validated_data):
+        job_source = validated_data.get("job_source")
+        job_link = validated_data.get("link")
+        self._validate_job_source_link(job_source, job_link)
+
+        return validated_data
+
+    def _validate_job_source_link(self, job_source, job_link):
+        if not job_source or not job_link:
+            return
+
+        if not re.match(job_source.link_regex, job_link):
+            raise serializers.ValidationError(f"Not a valid URL for the selected Job Source: {job_source}")
 
 
 class TechStackSerializer(serializers.HyperlinkedModelSerializer):
