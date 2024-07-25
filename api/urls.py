19019b8c3449f07ef5c845d523e18921f897912c
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-07-25 04:06:46 -0700

    add drf browserable api auth

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,8 @@
+from django.urls import include, path
+
 from .jobs.urls import router as jobs_router
 
-urlpatterns = [*jobs_router.urls]
+urlpatterns = [
+    *jobs_router.urls,
+    path("auth/", include("rest_framework.urls", namespace="rest_framework")),
+]
