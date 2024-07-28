40200fad07964bfaafe67f54c066d38f7c200d26
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-07-28 05:51:04 -0700

    JobViewSet: add permissions

--- api/jobs/viewsets.py
+++ api/jobs/viewsets.py
@@ -1,4 +1,4 @@
-from rest_framework import filters, viewsets
+from rest_framework import filters, permissions, viewsets
 
 from core.api.pagination import CustomCursorPagination
 from core.api.permissions import CustomModelPermissions
@@ -13,6 +13,7 @@ from .serializers import JobSerializer, TechStackSerializer
 class JobViewSet(viewsets.ModelViewSet):
     serializer_class = JobSerializer
     queryset = Job.objects.all().order_by("-created_at")
+    permission_classes = (permissions.DjangoModelPermissions,)
 
 
 class TechStackViewSet(viewsets.ModelViewSet):
