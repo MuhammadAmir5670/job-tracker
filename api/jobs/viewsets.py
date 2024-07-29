ab66cc86d4b131addd82b907ad4c3c1db7c6ad6a
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-07-29 14:01:05 -0700

    use `CustomModelPermissions`

--- api/jobs/viewsets.py
+++ api/jobs/viewsets.py
@@ -1,19 +1,16 @@
-from rest_framework import filters, permissions, viewsets
+from rest_framework import filters, viewsets
 
 from core.api.pagination import CustomCursorPagination
 from core.api.permissions import CustomModelPermissions
-from jobs.models import TechStack
-
 from jobs.models import Job, TechStack
 
-from .serializers import TechStackSerializer
 from .serializers import JobSerializer, TechStackSerializer
 
 
 class JobViewSet(viewsets.ModelViewSet):
     serializer_class = JobSerializer
     queryset = Job.objects.all().order_by("-created_at")
-    permission_classes = (permissions.DjangoModelPermissions,)
+    permission_classes = (CustomModelPermissions,)
     filter_backends = [filters.SearchFilter]
     search_fields = ["title", "company"]
 
