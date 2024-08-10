8272e27a2e0935f3314d23c52e876bd7ac054ffc
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-08-10 13:09:55 -0700

    feedback:nit: rearrange fields in `JobForm`

--- jobs/forms.py
+++ jobs/forms.py
@@ -13,7 +13,7 @@ class TechStackWidget(TomSelectMultipleWidget):
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
-        fields = ("company", "job_source", "link", "status", "tech_stacks", "title")
+        fields = ("title", "company", "job_source", "link", "status", "tech_stacks")
         widgets = {
             "tech_stacks": TechStackWidget,
             "link": forms.TextInput,
