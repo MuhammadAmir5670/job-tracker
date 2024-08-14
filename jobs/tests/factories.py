f22b1c84089962c1ffd03b7186cb879355396403
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-08-14 15:20:51 -0700

    refactor: move TechStackFactory to `tests.factories.py`

--- jobs/tests/factories.py
+++ jobs/tests/factories.py
@@ -1,11 +1,49 @@
 from random import choice
 
+import factory
 from django.utils import timezone
 from factory import Faker, LazyFunction, SubFactory
 from factory.django import DjangoModelFactory
+from faker.providers import DynamicProvider
 
 from accounts.tests.factories import UserFactory
-from jobs.models import Job
+from jobs.models import Job, TechStack
+
+tech_stack_provider = DynamicProvider(
+    provider_name="tech_stack",
+    elements=[
+        "LAMP",
+        "MEAN",
+        "MERN",
+        "JAMstack",
+        ".NET",
+        "Ruby on Rails",
+        "Django",
+        "Spring",
+        "Flutter",
+        "React Native",
+        "Vue.js",
+        "Serverless",
+        "ELK",
+        "ELT",
+        "Hadoop",
+        "Kubernetes",
+        "TensorFlow",
+        "PyTorch",
+        "Blockchain",
+        "GraphQL",
+        "Redis",
+    ],
+)
+factory.Faker.add_provider(tech_stack_provider)
+
+
+class TechStackFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = TechStack
+        django_get_or_create = ("name",)
+
+    name = factory.Faker("tech_stack")
 
 
 class JobFactory(DjangoModelFactory):
