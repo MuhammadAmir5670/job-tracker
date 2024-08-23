0474721fb53dec2b80b2999000ca463e00caa754
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2024-08-23 08:03:48 -0700

    migrate: change job description field type to  RichTextField

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,4 @@
+from ckeditor.fields import RichTextField
 from django.conf import settings
 from django.core.validators import URLValidator
 from django.db import models
@@ -47,7 +48,7 @@ class Job(TimeStampedModel):
     title = models.CharField(max_length=500)
     company = models.CharField(max_length=500)
     status = models.CharField(max_length=200, choices=Status.choices, default=Status.WISHLIST)
-    description = models.TextField(default="")
+    description = RichTextField(blank=True)
     link = models.TextField(validators=[URLValidator()])
     expired_at = models.DateTimeField(editable=False, null=True)
     applied_at = models.DateTimeField(auto_now_add=True)
